0075d9c03631b68d36ba8898eecbe3036b73a584
kyo <choiykyo@gmail.com>
2024-03-04 22:29:56 +0900

    S4_L10~L11_CBV

--- mydjango04/hottrack/urls.py
+++ mydjango04/hottrack/urls.py
@@ -10,6 +10,22 @@ urlpatterns = [
     re_path(
         route=r"^export\.(?P<format>(csv|xlsx))$", view=views.export, name="export"
     ),
-    path(route="archives/<date:release_date>/", view=views.index),
+    # path(route="archives/<date:release_date>/", view=views.index),
     path(route="<int:pk>/", view=views.song_detail),
+    path(route="melon-<int:melon_uid>/", view=views.song_detail),
+    path(
+        route="archives/<int:year>/",
+        view=views.SongYearArchiveView.as_view(),
+        name="song_archive_year",
+    ),
+    path(
+        route="archives/<int:year>/<int:month>/",
+        view=views.SongMonthArchiveView.as_view(),
+        name="song_archive_month",
+    ),
+    path(
+        route="archives/<int:year>/<int:month>/<int:day>/",
+        view=views.SongDayArchiveView.as_view(),
+        name="song_archive_day",
+    ),
 ]
--- mydjango04/hottrack/views.py
+++ mydjango04/hottrack/views.py
@@ -5,36 +5,74 @@ from urllib.request import urlopen
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 from django.db.models import QuerySet, Q
-from django.views.generic import DetailView
 from hottrack.models import Song
 from hottrack.utils.cover import make_cover_image
 from io import BytesIO
 from typing import Literal
 import pandas as pd
+from django.views.generic import (
+    ListView,
+    DetailView,
+    YearArchiveView,
+    MonthArchiveView,
+    DayArchiveView,
+)
 
 
-def index(request: HttpRequest, release_date: datetime.date = None) -> HttpResponse:
-    query = request.GET.get("query", "").strip()
-    song_qs: QuerySet = Song.objects.all()
+class SongListView(ListView):
+    model = Song
+    template_name = "hottrack/index.html"
+    paginate_by = 10
 
-    if query:
-        song_qs = song_qs.filter(
-            Q(name__icontains=query)
-            | Q(artist_name__icontains=query)
-            | Q(album_name__icontains=query)
-        )
+    query = None
 
-    if release_date:
-        song_qs = song_qs.filter(release_date=release_date)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        release_date = self.kwargs.get("release_date")
+        if release_date:
+            qs = qs.filter(release_date=release_date)
 
-    return render(
-        request=request,
-        template_name="hottrack/index.html",
-        context={
-            "song_list": song_qs,
-            "query": query,
-        },
-    )
+        self.query = self.request.GET.get("query", "").strip()
+        if self.query:
+            qs = qs.filter(
+                Q(name__icontains=self.query)
+                | Q(artist_name__icontains=self.query)
+                | Q(album_name__icontains=self.query)
+            )
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["total"] = context_data["object_list"].count()
+
+        context_data["query"] = self.query
+        return context_data
+
+
+index = SongListView.as_view()
+
+# def index(request: HttpRequest, release_date: datetime.date = None) -> HttpResponse:
+#     query = request.GET.get("query", "").strip()
+#     song_qs: QuerySet = Song.objects.all()
+
+#     if query:
+#         song_qs = song_qs.filter(
+#             Q(name__icontains=query)
+#             | Q(artist_name__icontains=query)
+#             | Q(album_name__icontains=query)
+#         )
+
+#     if release_date:
+#         song_qs = song_qs.filter(release_date=release_date)
+
+#     return render(
+#         request=request,
+#         template_name="hottrack/index.html",
+#         context={
+#             "song_list": song_qs,
+#             "query": query,
+#         },
+#     )
 
 
 def cover_png(request, pk):
@@ -108,4 +146,37 @@ def export(request: HttpRequest, format: Literal["csv", "xlsx"]) -> HttpResponse
     return response
 
 
-song_detail = DetailView.as_view(model=Song)
+class SongDetailView(DetailView):
+    model = Song
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        melon_uid = self.kwargs.get("melon_uid")
+        if melon_uid:
+            return get_object_or_404(queryset, melon_uid=melon_uid)
+        return super().get_object(queryset=queryset)
+
+
+song_detail = SongDetailView.as_view()
+
+
+class SongYearArchiveView(YearArchiveView):
+    model = Song
+    date_field = "release_date"
+
+    year = None
+    make_object_list = True
+
+
+class SongMonthArchiveView(MonthArchiveView):
+    model = Song
+    date_field = "release_date"
+    month_format = "%m"
+
+
+class SongDayArchiveView(DayArchiveView):
+    model = Song
+    date_field = "release_date"
+    month_format = "%m"
